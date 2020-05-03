eac59693e789f96cb40b6107b4da97d2dd7a8d7c
Francisco Sammartino <frperez@itba.edu.ar>
2020-05-03 13:52:44 -0300

    updated versioning

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import itertools as itertools
 
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
@@ -10,18 +11,25 @@ import regex as re
 
 INPUT_PARAMS_COUNT = 3
 
+#returns the versioned name in case filename already existed.
+def unique_file(name):
+	basename, ext = os.path.splitext(name)
+	actualname = "%s%s" % (basename, ext)
+	c = itertools.count()
+	next(c)
+	while os.path.exists(actualname):
+		actualname = "%s (%d)%s" % (basename, next(c), ext)
+
+	return actualname
+
+#Processing starts
 if len(sys.argv) != INPUT_PARAMS_COUNT:
 	print("Invalid params: 1) Input file path 2) Output file path")
 	sys.exit(1)
 
 input_file_name = sys.argv[1]
-output_file_name = sys.argv[2]
-
 # if output file name already exists we append a number '(n)' indicating version
-file_count = 1
-while os.path.exists(output_file_name) :
-	output_file_name = sys.argv[2] + "(" + str(file_count) + ")"
-	file_count += 1
+output_file_name = unique_file(sys.argv[2])
 
 # we are analyzing just one gene for this disease, but the algorithm can process multiple genes.
 records = SeqIO.parse(input_file_name, "gb")
